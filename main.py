5d577c18434a9bf8ec5b2025ba7a625c8760f460
Shunsuke Mie <sux2mfgj@gmail.com>
2018-10-29 17:53:11 +0900

    add a value for capasity

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from functools import reduce
 import copy
 
-def objective(values, number_of_items):
-    return reduce(
+def objective(capasity, values, number_of_items):
+    result = reduce(
             lambda s, x: s + x[0] * x[1],
             zip(values, number_of_items),
             0)
+    return (capasity >= result, result)
 
 def bruteforce(values, number_of_items):
-    number_of_items[0] += 1
+    pass
 
 if __name__ == '__main__':
 
     values = [120, 130, 80, 100, 250, 185]
     number_of_items = [0 for i in range(0, len(values))]
+    capasity = 65
     #result = bruteforce(values, copy.deepcopy(number_of_items))
-    #print(objective(values, number_of_items))
+    print(objective(capasity, values, number_of_items))
     #print(number_of_items)
