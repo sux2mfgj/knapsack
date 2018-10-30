c9cd6af91b6559f239570fd2744060e243cdbdf8
Shunsuke Mie <sux2mfgj@gmail.com>
2018-10-30 18:46:32 +0900

    run standalone

--- iterative_local_search.py
+++ iterative_local_search.py
@@ -1,3 +1,4 @@
+from logging import basicConfig
 from utils import Solver, objective
 
 
@@ -9,3 +10,22 @@ class IterativeLocalSearch(Solver):
 
     def solve(self):
         return self.number_of_items
+
+
+if __name__ == '__main__':
+    # Parameters
+    values = [120, 130, 80, 100, 250, 185]
+    costs = [10, 12, 7, 9, 21, 16]
+    number_of_items = [0 for i in range(0, len(values))]
+    capasity = 100
+
+    # for logging
+    from logging import DEBUG
+    basicConfig(level=DEBUG)
+
+    sa = IterativeLocalSearch(capasity, values, costs, number_of_items, 2)
+    best_result_of_sa = sa.solve()
+    best_cost = objective(capasity, values, costs, best_result_of_sa)
+
+    print('best conbination: ', best_result_of_sa)
+    print('best cost:', best_cost)
